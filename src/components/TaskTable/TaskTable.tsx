cba5254d764a3e29432e4ece9c5c8b07980309d7
Samuel Ruiz <Samuel.Ruiz@integritymarketing.com>
2023-03-24 16:12:39 -0500

    fix html table issue

--- src/components/TaskTable/TaskTable.tsx
+++ src/components/TaskTable/TaskTable.tsx
@@ -4,15 +4,25 @@ import { appRoutes } from "../../routes";
 const TaskTable = () => {
   return (
     <table>
-      <tr>
-        <th>Name</th>
-      </tr>
-      <tr>
-        <Link to={appRoutes.taskItem.path.replace(":taskId", "483gew")}>New Task 483gew</Link>
-      </tr>
-      <tr>
-        <Link to={appRoutes.taskItem.path.replace(":taskId", "123abc")}>New Task 123abc</Link>
-      </tr>
+      <tbody>
+        <tr>
+          <th>Name</th>
+        </tr>
+        <tr>
+          <td>
+            <Link to={appRoutes.taskItem.path.replace(":taskId", "483gew")}>
+              New Task 483gew
+            </Link>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <Link to={appRoutes.taskItem.path.replace(":taskId", "123abc")}>
+              New Task 123abc
+            </Link>
+          </td>
+        </tr>
+      </tbody>
     </table>
   );
 };
