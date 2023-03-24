b22b345c3af1f2a6d20b71f3ab09de581919dab5
Samuel Ruiz <Samuel.Ruiz@integritymarketing.com>
2023-03-24 16:15:49 -0500

    log messages

--- src/eventBus.ts
+++ src/eventBus.ts
@@ -3,17 +3,20 @@ export class EventBus {
   baseApp: any;
   baseAppURL: string | undefined;
   constructor() {
+    console.log("script: Task module listening for events...")
     window.addEventListener("message", this.receiveMessage);
   }
 
   // TODO: set event  type
   receiveMessage = (event: any) => {
+    console.log("script: Task module message receive, ", event.origin, event.source)
     if (event.origin === window.location.origin) return;
     this.baseApp = event.source;
     this.baseAppURL = event.origin;
   };
 
   sendMessage = (message: object) => {
+    console.log("script: Task module send message: ", message)
     if (this.baseApp) {
       this.baseApp.postMessage(message, this.baseAppURL);
     }
